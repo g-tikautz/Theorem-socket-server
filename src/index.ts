f7b59ef58ac0a0bc9bc6f7374f4a280e6c1a6075
ToyotaKakashi <gregor.tikautz@pcode.at>
2022-05-08 19:56:46 +0200

    Add private GameRooms

--- src/index.ts
+++ src/index.ts
@@ -66,29 +66,56 @@ server.listen(3000, () => {
   console.log("listening on *:3000");
 });
 
-io.on("connection", (socket: Socket) => {
-  console.log(socket.id);
+let roomId:string | string[] | undefined = undefined;
+let isPrivate:string | string[] | undefined = undefined;
 
-  if (freeGameRooms.length == 0) {
-    let gameRoom = new GameRoom();
-    gameRoom.gameroomId = generateGameRoomID();
-    gameRoom.player1 = socket.id;
-    gameRooms.set(gameRoom.gameroomId, gameRoom);
-    freeGameRooms.push(gameRoom.gameroomId);
-    socket.emit("gameRoomID", gameRoom.gameroomId);
+io.use((socket, next) => {
+  roomId = socket.handshake.query.roomId;
+  isPrivate = socket.handshake.query.isPrivate;
+  if (socket.handshake.query.token === "UNITY") {
+      next();
   } else {
-    let gameRoom = gameRooms.get(freeGameRooms.splice(0, 1)[0]);
-    gameRoom!.player2 = socket.id;
-    gameRoom!.status = GameStatus.playing;
-    socket.emit("gameRoomID", gameRoom!.gameroomId);
-    let random_boolean = Math.random() < 0.5;
-    io.sockets.sockets
-      .get(gameRoom!.player1)
-      ?.emit("startGame", random_boolean);
-    io.sockets.sockets
-      .get(gameRoom!.player2)
-      ?.emit("startGame", !random_boolean);
+      next(new Error("Authentication error"));
   }
+});
+
+io.on("connection", (socket: Socket) => {
+console.log(roomId);
+console.log(isPrivate);
+ if(roomId){
+    joinGameRoom(socket, roomId as string);
+ }else{
+   if(isPrivate){
+     createPrivateRoom(socket);
+   }else{
+    normalSearch(socket);
+   }
+ }
+
+ socket.on("showCardsFighting",(roomId: string,x:number,y:number,z:number,dies:boolean,attackDamage:number,defenseDamage:number,effects:number[],x2:number,y2:number,z2:number,dies2:boolean,attackDamage2:number,defenseDamage2:number,effects2:number[]) => {   
+  if(gameRooms.get(roomId)!.player1 == socket.id){
+    io.sockets.sockets
+    .get(gameRooms.get(roomId)!.player2)
+      ?.emit("enemyAttackableStateChange", roomId,x,y,z,dies,attackDamage,defenseDamage,effects,x2,y2,z2,dies2,attackDamage2,defenseDamage2,effects2);
+  }else{
+    io.sockets.sockets
+    .get(gameRooms.get(roomId)!.player1)
+      ?.emit("enemyAttackableStateChange", roomId,x,y,z,dies,attackDamage,defenseDamage,effects,x2,y2,z2,dies2,attackDamage2,defenseDamage2,effects2);
+  }
+ });
+
+ socket.on("showUserAttack",(roomId: string,x: number,y:number,z:number,value:number)=>{
+    
+    if(gameRooms.get(roomId)!.player1 == socket.id){
+        io.sockets.sockets
+        .get(gameRooms.get(roomId)!.player2)
+          ?.emit("enemyAttackableStateChange", x,y,z,value);
+    }else{
+        io.sockets.sockets
+        .get(gameRooms.get(roomId)!.player1)
+          ?.emit("enemyAttackableStateChange", x,y,z,value);
+    }
+ });
 
   socket.on(
     "enemyAttackableStateChange",
@@ -175,8 +202,56 @@ io.on("connection", (socket: Socket) => {
   socket.on("disconnect", () => {
     console.log("user disconnected");
   });
-
-  socket.on("chat message", (msg) => {
-    console.log("message: " + msg);
-  });
 });
+
+function normalSearch(socket: Socket) {
+  
+  if (freeGameRooms.length == 0) {
+    let gameRoom = new GameRoom();
+    gameRoom.gameroomId = generateGameRoomID();
+    gameRoom.player1 = socket.id;
+    gameRooms.set(gameRoom.gameroomId, gameRoom);
+    freeGameRooms.push(gameRoom.gameroomId);
+    socket.emit("gameRoomID", gameRoom.gameroomId);
+  } else {
+    let gameRoom = gameRooms.get(freeGameRooms.splice(0, 1)[0]);
+    gameRoom!.player2 = socket.id;
+    gameRoom!.status = GameStatus.playing;
+    socket.emit("gameRoomID", gameRoom!.gameroomId);
+    let random_boolean = Math.random() < 0.5;
+    io.sockets.sockets
+      .get(gameRoom!.player1)
+      ?.emit("startGame", random_boolean);
+    io.sockets.sockets
+      .get(gameRoom!.player2)
+      ?.emit("startGame", !random_boolean);
+  }
+}
+
+function joinGameRoom(socket: Socket, roomId: string){
+  let gameRoom = gameRooms.get(roomId);
+  if(gameRoom){
+    gameRoom.player2 = socket.id;
+    gameRoom.status = GameStatus.playing;
+    socket.emit("gameRoomID", gameRoom.gameroomId);
+    let random_boolean = Math.random() < 0.5;
+    io.sockets.sockets
+      .get(gameRoom.player1)
+      ?.emit("startGame", random_boolean);
+    io.sockets.sockets
+      .get(gameRoom.player2) 
+      ?.emit("startGame", !random_boolean);
+  }else{
+    console.log("gameRoom not found");
+    socket.disconnect();
+  }
+}
+
+function createPrivateRoom(socket: Socket) {
+  console.log("yeah");
+  let gameRoom = new GameRoom();
+  gameRoom.gameroomId = generateGameRoomID();
+  gameRoom.player1 = socket.id;
+  gameRooms.set(gameRoom.gameroomId, gameRoom);
+  socket.emit("gameRoomID", gameRoom.gameroomId);
+}
